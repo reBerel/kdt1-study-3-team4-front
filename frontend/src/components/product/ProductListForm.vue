73c50fd0aedc38fceca5d9a547068d82bb3f2e74
cluypkoj <128336184+cluypkoj@users.noreply.github.com>
2023-05-25 22:50:29 +0900

    update: [백지우] 상품 리스트 수정 1차 [Productx]

---
+++ frontend/src/components/product/ProductListForm.vue
@@ -0,0 +1,73 @@
+<template lang="">
+    <div>
+        <h3>상품 목록</h3>
+        <p></p>
+        <table>
+            <tr>
+                <td align="center" width="2%">상품명</td>
+                <td align="center" width="2%">상품가격</td>
+                <td align="center" width="2%">제조사</td>
+                <td align="center" width="2%">주문날짜</td>
+                <td align="center" width="2%">배송정보</td>
+            </tr>
+            <p></p>
+            <tr v-if="!products || (Array.isArray(products) && products.length === 0)">
+                <td colspan="5">
+                    현재 등록된 상품이 없습니다!
+                </td>
+            </tr>
+            <tr v-else v-for="product in products" :key="product.productId">
+                <td>
+                    <router-link :to="{
+                        name: 'ProductReadPage',
+                        params: { productId: product.productId.toString() }}">
+                        {{ product.name }}
+                    </router-link>
+                </td>
+                <td>
+                    {{ product.price }}
+                </td>
+                <td>
+                    {{ product.company }}
+                </td>
+                <td>
+                    {{ product.orderDate }}
+                </td>
+                <td>
+                    {{ product.details }}
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        products: {
+            type: Array
+        }
+    }
+
+}
+</script>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Song+Myung&display=swap');
+
+h3 {
+    color: darkslateblue;
+    font-family: 'Song Myung', serif;
+    text-shadow: 4px 4px 4px cornflowerblue;
+    font-size: 40px;
+}
+
+table {
+    border: 4px solid mediumpurple;
+    font-family: 'Song Myung', serif;
+    font-size: 20px;
+}
+
+td {
+    border: 2px solid lightskyblue;
+}
+</style>
--- frontend/src/views/product/ProductListPage.vue
+++ frontend/src/views/product/ProductListPage.vue
@@ -1,80 +1,38 @@
-<template>
+<template lang="">
     <div>
-        <table>
-        <tr>
-            <td>상품 번호</td>
-            <td>
-                <input type="text" :value="productId" readonly/>
-            </td>
-        </tr>
-        <tr>
-            <td>상품명</td>
-            <td>
-                <input type="text" :value="productName" readonly/>
-            </td>
-        </tr>
-        <tr>
-            <td>가격</td>
-            <td>
-                <input type="number" :value="productPrice" readonly/>
-            </td>
-        </tr>
-        <tr>
-          <td>세부 사항</td>
-            <td>
-                <textarea cols="50" rows="20" :value="productDetails" readonly/>
-            </td>
-        </tr>
-    </table>
-
-    <v-row>
-        <v-col v-for="(imagePath, idx) in productImagesPathList" :key="idx" cols="12">
-            <v-img :src="require(`@/assets/uploadImgs/${imagePath}`)" aspect-ratio="1" class="grey lighten-2">
-                <template v-slot:placeholder>
-                    <v-row class="fill-height ma-0" align="center" justify="center">
-                        <v-progress-circular indeterminate color="grey lighten-5"/>
-                    </v-row>
-                </template>
-            </v-img>
-      </v-col>
-    </v-row>
-
-  </div>
+        <h2>상품 게시판</h2>        
+        <p></p>
+        <product-list-form :products="products"/>
+        <div  align="center">
+            <p></p>
+            <router-link :to="{ name: 'ProductRegisterPage'}">
+                상품 등록
+            </router-link>
+        </div>
+    </div>
 </template>
-
 <script>
-import axiosInst from '@/utility/axiosInst';
+
+import ProductListForm from '@/components/product/ProductListForm.vue';
+import { mapActions, mapState } from 'vuex';
+
+const productModule = 'productModule'
 
 export default {
-    data () {
-        return {
-            productId: 0,
-            productName: '',
-            productPrice: 0,
-            productDetails: '',
-            productImagesPathList: null
-        }
+    components: { ProductListForm },
+    computed: {
+        ...mapState(productModule, ['products'])
     },
     methods: {
-        setData (payload) {
-            this.productId = payload.id
-            this.productName = payload.productName
-            this.productPrice = payload.productPrice
-            this.productDetails = payload.productDetails
-            this.productImagesPathList = payload.productImagesPathList
-        }
+        ...mapActions(
+            productModule, ['requestProductListToSpring']
+        )
     },
-    async mounted () {
-        const product = await axiosInst.get('/product/read/3')
-
-        console.log('product: ' + JSON.stringify(product.data))
-
-        await this.setData (product.data)
+    mounted() {
+        this.requestProductListToSpring()
     }
 }
-  
 </script>
-
-<style>
-  
+<style lang="">
+    
 </style>
