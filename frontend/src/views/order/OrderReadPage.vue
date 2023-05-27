b30431cc5664461b057148ac7c1d9f47339daaa6
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-05-28 06:36:34 +0900

    feat: OrderRead를 위한 페이지 생성 [ORDER_READ-1]
    OrderRead 페이지를 생성합니다.

---
+++ frontend/src/views/order/OrderReadPage.vue
@@ -0,0 +1,60 @@
+<template>
+    <div style="text-align: center;">
+        <div :style="{ marginTop: '50px' , marginBottom: '50px'}">
+            <h2>주문 내역</h2>
+        </div>
+        <div class="centered" style="text-align: center;">
+            <order-read-form v-if="order" :order="order"/>
+            <p v-else>기다려주세요.</p>
+        </div>
+        <p></p>
+        <div style="text-align: center;">
+            <router-link :to="{ name: 'OrderHistoryPage' }">
+                <v-btn color="gray" dark style="margin: 0px 5px 0px 0px;">뒤로가기</v-btn>
+            </router-link>
+            <v-btn color="red" dark @click="onDelete">주문취소</v-btn>
+        </div>
+    </div>
+</template>
+
+<script>
+import OrderReadForm from '@/components/order/OrderReadForm.vue'
+import { mapActions, mapState } from 'vuex'
+
+const orderModule = 'orderModule'
+
+export default {
+    components: {
+        OrderReadForm,
+    },
+    // props: {
+    //     orderId: {
+    //         type: String,
+    //         required: true,
+    //     },
+    // },
+    computed: {
+        ...mapState(orderModule, ['order'])
+    },
+    methods: {
+        ...mapActions(
+            orderModule, ['requestOrderToSpring', 'requestDeleteOrderSpring']
+        ),
+        async onDelete () {
+            await this.requestDeleteOrderSpring(this.$router.currentRoute.params.orderId)
+            await this.$router.push({ name: 'OrderHistoryPage' })
+        }
+    },
+    created () {
+        this.requestOrderToSpring(this.$router.currentRoute.params.orderId)
+    }
+}
+</script>
+
+<style>
+    .centered {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
