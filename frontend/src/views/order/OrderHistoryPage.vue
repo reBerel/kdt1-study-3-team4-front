d315709c516a31f990a95066c9e603d3d2f7b546
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-05-28 06:33:19 +0900

    feat: [장현민] OrderHistory 를 사용하기 위한 페이지 생성 [ORDER_HISTORY-1]
    OrderHistoryPage입니다.

---
+++ frontend/src/views/order/OrderHistoryPage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div style="text-align: left; margin: 20px;">
+        </div>
+        <order-history-form :orders="orders"/>
+    </div>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex';
+import OrderHistoryForm from '@/components/order/OrderHistoryForm.vue'
+const orderModule = 'orderModule'
+
+export default {
+    components: { OrderHistoryForm},
+    computed: {
+        ...mapState(orderModule, ['orders']),
+    },
+    mounted () {
+        this.requestOrderHistoryToSpring(1)
+    },
+    methods: {
+        ...mapActions(
+            orderModule, ['requestOrderHistoryToSpring']
+        )
+    }
+}
+</script>
+
+<style>
+
+</style>
