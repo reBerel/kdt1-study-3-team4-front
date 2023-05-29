014971f379791602cb06b961d3ba9d05c95f20d3
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-05-29 22:46:30 +0900

    feat: [남현기] 상품 정보 수정 기능 구현 [VPP-MODIFY-1]

--- frontend/src/router/product/index.js
+++ frontend/src/router/product/index.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router'
 import ProductRegisterPage from '@/views/product/ProductRegisterPage.vue'
 import ProductListPage from '@/views/product/ProductListPage.vue'
 import ProductReadPage from '@/views/product/ProductReadPage.vue'
+import ProductModifyPage from '@/views/product/ProductModifyPage.vue'
 
 Vue.use(VueRouter)
 
@@ -26,6 +27,16 @@ const productRoutes = [
     },
     props: {
       default: true
+    }
+  },
+  {
+    path: '/product-modify-page/:productId',
+    name: 'ProductModifyPage',
+    components: {
+      default: ProductModifyPage
+    },
+    props: {
+      default: true,
     }
   },
 
