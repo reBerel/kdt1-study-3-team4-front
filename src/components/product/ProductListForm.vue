bbd44e527b8a3436f5527a3ad23f42a3b9964ce4
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-06-02 17:40:15 +0900

    Revert "fix: [남현기] 이미지 사용 주석처리"
    This reverts commit a6a928cfa074aff6507bb74feccb2acf77818d14.

--- src/components/product/ProductListForm.vue
+++ src/components/product/ProductListForm.vue
@@ -4,18 +4,18 @@
         <p></p>
         <table>
             <tr>
-                <!-- <td align="center" width="2%">상품 이미지</td> -->
+                <td align="center" width="2%">상품 이미지</td>
                 <td align="center" width="2%">상품명</td>
                 <td align="center" width="2%">상품가격</td>
             </tr>
             <p></p>
             <tr v-if="!products || (Array.isArray(products) && products.length === 0)">
-                <td colspan="2">
+                <td colspan="3">
                     현재 등록된 상품이 없습니다!
                 </td>
             </tr>
             <tr v-else v-for="product in products" :key="product.productId">
-                <!-- <td>
+                <td>
                     <v-img :src="require(`@/../../../kdt1-study-3-team4-back/demo/src/main/java/com/example/demo/UploadImgs/${product.productImagesPathList[0]}`)" aspect-ratio="1" class="grey lighten-2">
                         <template v-slot:placeholder>
                             <v-row class="fill-height ma-0" align="center" justify="center">
@@ -23,7 +23,7 @@
                             </v-row>
                         </template>
                     </v-img>
-                </td> -->
+                </td>
                 <td>
                     <router-link :to="{
                         name: 'ProductReadPage',
